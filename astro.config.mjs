2428f2d67c3966a66cf7afd4fbf1340c433959b0
André Silva <andre.silva@posteo.de>
2024-07-04 20:14:45 +1000

    Delete duplicate import in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
-
   integrations: [tailwind(), icon(), sitemap(), react()],
-  site: "https://www.hinterlandweb.com"
-
+  site: "https://www.hinterlandweb.com",
 });
