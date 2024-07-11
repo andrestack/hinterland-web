f07a761c98a2b8842b911c3a9f361d167e4c2bed
André Silva <andre.silva@posteo.de>
2024-07-11 16:16:58 +1000

    Add kindy website to portfolio

--- src/utils/work.ts
+++ src/utils/work.ts
@@ -14,11 +14,36 @@ const projects = [
     url: "https://wayfaring.hinterlandweb-folio.in/",
   },
   {
-    title: "Sala",
-    description: "A invoicing solution for online tutors",
-    image: `/images/screenshot-sala.png`,
-    url: "https://final-project-sala.vercel.app/",
+    title: "Garden Gnome",
+    description: "A simple 3 page Website for a local landscaping business.",
+    image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/garnden-gnome-hero.png`,
+    url: "https://gardengnome.hinterlandweb-folio.in/",
   },
+  {
+    title: "Zimmermann Roofing",
+    description: "A website template I created for a local roofing business.",
+    image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/roofing.png`,
+    url: "https://roofing.hinterlandweb-folio.in/",
+  },
+  {
+    title: "Pohsso Wellness",
+    description: "Soon launching a website for a masseuse friend in Portugal",
+    image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/wellness-booking-calendar.png`,
+    url: "https://wellness.hinterlandweb-folio.in/",
+  },
+  {
+    title: "Automation Agency",
+    description: "An alternative template I started developing for my agency",
+    image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/marketing-header.png`,
+    url: "https://marketing.hinterlandweb-folio.in/",
+  },
+  {
+    title: "Pomona & District Kindergarten",
+    description: "Website redesign for local Kindy in Pomona",
+    image: `https://pub-31971714d5324882b00b0345130560dd.r2.dev/pomona%20kindy.png`,
+    url: "https://hinterland-web.github.io/pomona-kindy/",
+  },
+
 
   ,
 ];
