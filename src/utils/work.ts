b06b1c22ba504b53791f8283cdb4f7a9653828c4
André Silva <124931972+andrestack@users.noreply.github.com>
2026-02-09 21:29:31 +1000

    remove redundant files

--- src/utils/work.ts
+++ src/utils/work.ts
@@ -5,10 +5,10 @@ export interface Project {
   link: string;
   technologies: string[];
   features: string[];
-  position: string;
+  position?: string;
 }
 
-const projects: Project[] = [
+export const projects: Project[] = [
   {
     title: "Riviera Renovations",
     description:
@@ -31,33 +31,54 @@ const projects: Project[] = [
   },
   {
     title: "Wayfaring",
-    description: "Recruitment agency based in Melbourne that wanted a more modern and professional design update for their website.",
+    description:
+      "Recruitment agency based in Melbourne that wanted a more modern and professional design update for their website.",
     image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/screenshot-wayfaring.png`,
     link: "https://wayfaring.hinterlandweb-folio.in/",
     technologies: ["HTML", "CSS", "hosted on Hostinger"],
-    features: ["Responsive Design", "Fast Loading", "SEO Optimized", "Custom Logo Design"],
+    features: [
+      "Responsive Design",
+      "Fast Loading",
+      "SEO Optimized",
+      "Custom Logo Design",
+    ],
     position: "center",
   },
   {
     title: "Garden Gnome",
-    description: "A simple 3 page Website for a local landscaping business. The website now provides all the necessary information for clients to make an informed decision when booking gardening services.",
+    description:
+      "A simple 3 page Website for a local landscaping business. The website now provides all the necessary information for clients to make an informed decision when booking gardening services.",
     image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/garnden-gnome-hero.png`,
     link: "https://gardengnome.hinterlandweb-folio.in/",
     technologies: ["HTML", "CSS", "hosted on Hostinger"],
-    features: ["Responsive Design", "Fast Loading", "SEO Optimized", "Onbrand Design"],
+    features: [
+      "Responsive Design",
+      "Fast Loading",
+      "SEO Optimized",
+      "Onbrand Design",
+    ],
   },
   {
     title: "True Fit Training",
-    description: "The client needed a full redesing of their existing website. It now provides a branding and a user experience that better reflect Richard Salmon's offering, focusing on the benegits of training, providing a lot of knowledge through a blog. The client also engaged my SEO services which include on page and off page optimisation, basic keyword research and a SEO strategy outline.",
+    description:
+      "The client needed a full redesing of their existing website. It now provides a branding and a user experience that better reflect Richard Salmon's offering, focusing on the benegits of training, providing a lot of knowledge through a blog. The client also engaged my SEO services which include on page and off page optimisation, basic keyword research and a SEO strategy outline.",
     image: `https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/Mockups.png`,
     link: "https://www.truefittraining.com.au/",
     technologies: ["Wix Studio", "Custom CSS", "hosted on Hostinger"],
-    features: ["Responsive Design", "Fast Loading", "SEO Optimized", "Onbrand Design", "Blog", "Google Reviews"],
-    position:"center",
+    features: [
+      "Responsive Design",
+      "Fast Loading",
+      "SEO Optimized",
+      "Onbrand Design",
+      "Blog",
+      "Google Reviews",
+    ],
+    position: "center",
   },
   {
     title: "Custom Registration Form",
-    description: "Developed a registration form for a dance and drum retreat in Portugal, called Aldeia Djembe Camp. Data is submitted to a database and then displayed in an admin dashboard.",
+    description:
+      "Developed a registration form for a dance and drum retreat in Portugal, called Aldeia Djembe Camp. Data is submitted to a database and then displayed in an admin dashboard.",
     image: `/images/adc-form.png`,
     link: "https://formulario.aldeia-djembe-camp.com/",
     technologies: ["Nextjs", "MongoDB", "React Hook Form", "Tailwind CSS"],
@@ -65,26 +86,47 @@ const projects: Project[] = [
   },
   {
     title: "Automation Agency",
-    description: "This is a work in progress for my agency. It is a full stack template for any SaaS or product website. It showcases some of the features we can integrate on a fully custom website.",
-    image: "https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/saas-website.png",
+    description:
+      "This is a work in progress for my agency. It is a full stack template for any SaaS or product website. It showcases some of the features we can integrate on a fully custom website.",
+    image:
+      "https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/saas-website.png",
     link: "https://saas-template-puce.vercel.app/",
     technologies: ["Nextjs", "Tailwind CSS", "Magic UI", "Deployed on Vercel"],
-    features: ["Responsive Design", "Micro Interactions", "Animations", "User Authentication", "Blog"],
+    features: [
+      "Responsive Design",
+      "Micro Interactions",
+      "Animations",
+      "User Authentication",
+      "Blog",
+    ],
     position: "center",
   },
   {
     title: "Pomona & District Kindergarten",
-    description: "As part of my volunteer work for the local community Kindy, I redesigned the Kindys website to provide a more modern and professional look. The website is the result of productive feedback and collaborations with the executive committee.",
-    image: "https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/Mockups_kindy.png",
+    description:
+      "As part of my volunteer work for the local community Kindy, I redesigned the Kindys website to provide a more modern and professional look. The website is the result of productive feedback and collaborations with the executive committee.",
+    image:
+      "https://pub-72408206c0cf4600bc497ef0bdbc7c1a.r2.dev/Mockups_kindy.png",
     link: "https://hinterland-web.github.io/pomona-kindy/",
-    technologies: ["Nextjs", "Tailwind CSS", "Relume UI", "Deployed on Github Pages"],
-    features: ["Responsive Design", "On Brand Design", "Custom Components", "Custom Enrolment Modal"],
+    technologies: [
+      "Nextjs",
+      "Tailwind CSS",
+      "Relume UI",
+      "Deployed on Github Pages",
+    ],
+    features: [
+      "Responsive Design",
+      "On Brand Design",
+      "Custom Components",
+      "Custom Enrolment Modal",
+    ],
     position: "center",
   },
- 
+
   {
     title: "Buckley's Chance",
-    description: "Website showcasing a local Pomona band, playing a mix of covers with a groovy celtic feel. The group only had flyers and a very outdated Facebook page, so I helped them by defining brand colors and also creating a logo. The website serves the simple purpose of presenting the band and providing contact details for any booking enquiries",
+    description:
+      "Website showcasing a local Pomona band, playing a mix of covers with a groovy celtic feel. The group only had flyers and a very outdated Facebook page, so I helped them by defining brand colors and also creating a logo. The website serves the simple purpose of presenting the band and providing contact details for any booking enquiries",
     image: `/images/bc-logo-no-bg.png`,
     link: "https://buckleyschance.net",
     technologies: ["HTML", "CSS", "hosted on Hostinger"],
@@ -93,14 +135,11 @@ const projects: Project[] = [
   },
   {
     title: "Bear Cave Barber",
-    description: "After buying the business from it previous owner, the client went through a complete rebranding of the business. My task was to create a simple but yet effective landing page, that provides all the relevant information about the barber shop, including products available and a link to Google Reviews.",
+    description:
+      "After buying the business from it previous owner, the client went through a complete rebranding of the business. My task was to create a simple but yet effective landing page, that provides all the relevant information about the barber shop, including products available and a link to Google Reviews.",
     image: `https://pub-ed0f4b8b153c4c809b08926b1d61a25e.r2.dev/bearcavebarber-website-screenshot.png`,
     link: "https://bearcavebarber.com.au",
     technologies: ["HTML", "CSS", "hosted on Hostinger"],
     features: ["Responsive design", "SEO Optimized", "Low Cost"],
   },
-
-  ,
 ];
-
-export { projects };
